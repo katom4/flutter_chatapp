15d8fda45c83a1814f741374dc90a9739ec33776
katomasatt <84952773+katomasatt@users.noreply.github.com>
2022-01-15 19:00:35 +0900

    reply1

--- lib/page/chatViewpage.dart
+++ lib/page/chatViewpage.dart
@@ -1,44 +1,116 @@
 import 'package:chat/importer.dart';
-import 'profilepage.dart';
-import 'loginpage.dart';
-import 'addPostPage.dart';
-import 'chatViewpage.dart';
+import 'package:chat/page/addPostreplyPage.dart';
+import 'chatpage.dart';
 
-class ChatPage extends ConsumerWidget{
-  ScrollController _controller=new ScrollController();
+final replyDateProvider = StateProvider((ref){
+return [""];
+});
+
+final replypathProvider = StateProvider((ref){
+  return "";
+});
+
+final replysProvider = StateProvider((ref){
+  return [];
+});
+
+final replysunamesProvider = StateProvider((ref){
+  return [];
+});
+
+final replyPageProvider = StateProvider.autoDispose((ref){
+  return 0;
+});
+
+final replyrouteProvider=StateProvider((ref){
+  dynamic a;
+  return a;
+});
+
+final replysfutureProvider = StateProvider.autoDispose.family<Future<List>,dynamic>((ref,reference)async{
+  if(ref.watch(replyrouteProvider.state).state!=reference||ref.watch(replyDateProvider.state).state=="")return [];
+  List<dynamic> datalist =[];
+    await reference
+                .orderBy('date',descending: true)
+                .limit(20)
+                .startAfter([ref.watch(replyDateProvider.state).state])
+                .get()
+                .then((value){
+                  
+                  for(var s in value.docs){
+                    datalist.add(s);
+                    ref.watch(replysProvider.state).state.add(s);
+                  }
+                });
+    for(var a in datalist){
+      String i = await getuname(a["uid"]);
+      ref.watch(replysunamesProvider.state).state.add(i);
+    }
+   return datalist;
+});
+class chatViewPage extends ConsumerWidget{
+  chatViewPage(this.data);
+  Map<String,dynamic> data;
+  
   @override
   Widget build(BuildContext context, WidgetRef ref){
-    final User user=ref.watch(userProvider.state).state!;
-    _controller.addListener(() { 
-      if (_controller.hasClients){
-         _controller.jumpTo(0);
-         print('err');
-      }
-    });
+
     return Scaffold(
       body:Center(
         child: Column(
           children: <Widget>[
             SizedBox(height:50),
+            Container(
+              child:Column(
+                children: <Widget>[
+                  Text(data['text']),
+                  Row(
+                    children: <Widget>[
+                      Text(getConversionDate(data['date'])),
+                      ElevatedButton(
+                        child: Text("リプ"),
+                        onPressed: ()async{
+                          await Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context){
+                              return AddPostreplyPage();
+                            })
+                          ).then((value){
+                            //ref.watch(replypathProvider.state).state="";
+                            ref.watch(replyrouteProvider.state).state=data['future'];
+                            ref.watch(replysProvider.state).state=[];
+                            ref.watch(replysunamesProvider.state).state=[];
+                            ref.watch(replyDateProvider.state).state=[DateTime.now().toLocal().toIso8601String()];
+                            ref.watch(replyPageProvider.state).state=0;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            
             Expanded(
               child:Scrollbar(
                 child: FutureBuilder(
-                  future:ref.watch(dProvider),
+                  future:ref.watch(replysfutureProvider(data['future'])),
                   builder:(BuildContext context,AsyncSnapshot<List<dynamic>> b){
                     return RefreshIndicator(
                       onRefresh: ()async{
-                        ref.watch(unamesProvider.state).state=[];
-                        ref.watch(daProvider.state).state=[];
-                        ref.watch(lastdateProvider.state).state=[DateTime.now().toLocal().toIso8601String()];
+                        ref.watch(replyrouteProvider.state).state=data['future'];
+                        ref.watch(replysProvider.state).state=[];
+                        ref.watch(replysunamesProvider.state).state=[];
+                        ref.watch(replyDateProvider.state).state=[DateTime.now().toLocal().toIso8601String()];
+                        ref.watch(replyPageProvider.state).state=0;
                       },
                       child:ListView.builder(
-                        itemCount: ref.watch(daProvider.state).state.length,
+                        itemCount: ref.watch(replysProvider.state).state.length,
                         itemBuilder:(context,i){
                           Future<String> a;
                           WidgetsBinding.instance!.addPostFrameCallback((_) async{
-                          if(i%19==0&&i!=0&&ref.watch(nowPageProvider.state).state<i){
-                              ref.watch(lastdateProvider.state).state=[ref.watch(daProvider.state).state[i]['date']];
-                              ref.watch(nowPageProvider.state).state=i;
+                          if(i%19==0&&i!=0&&ref.watch(replyPageProvider.state).state<i){
+                              ref.watch(replyDateProvider.state).state=[ref.watch(replysProvider.state).state[i]['date']];
+                              ref.watch(replyPageProvider.state).state=i;
                             }
                           });
                           if(b.connectionState==ConnectionState.done||b.connectionState==ConnectionState.waiting){
@@ -58,12 +130,12 @@ class ChatPage extends ConsumerWidget{
                                         children:[
                                           Row(
                                             children: [
-                                              Text(ref.watch(unamesProvider.state).state[i]+"　　",
+                                              Text(ref.watch(replysunamesProvider.state).state[i]+"　　",
                                                       style: TextStyle(fontWeight: FontWeight.bold),),
-                                              Text(getConversionDate(ref.watch(daProvider.state).state[i]["date"])),
+                                              Text(getConversionDate(ref.watch(replysProvider.state).state[i]["date"])),
                                             ],
                                           ),
-                                          Text(ref.watch(daProvider.state).state[i]["text"])
+                                          Text(ref.watch(replysProvider.state).state[i]["text"]),
                                         ]
                                       ),
                                     ],
@@ -105,13 +177,13 @@ class ChatPage extends ConsumerWidget{
                                             children: [
                                               Text(ref.watch(unamesProvider.state).state[i]+"　　",
                                                     style: TextStyle(fontWeight: FontWeight.bold),),
-                                              Text(getConversionDate(ref.watch(daProvider.state).state[i]["date"])),
+                                              Text(getConversionDate(ref.watch(replysProvider.state).state[i]["date"])),
                                             ],
                                           )
                                         ),
                                         Container(
                                           alignment: Alignment.centerLeft,
-                                          child:Text(ref.watch(daProvider.state).state[i]["text"]),
+                                          child:Text(ref.watch(replysProvider.state).state[i]["text"]),
                                         ),
                                       ],
                                     ),
@@ -120,31 +192,35 @@ class ChatPage extends ConsumerWidget{
                               ],
                             ),
                             onTap: ()async{
-                              ref.watch(replypathProvider.state).state=ref.watch(daProvider.state).state[i].id;
-                               ref.watch(replyDateProvider.state).state=[DateTime.now().toLocal().toIso8601String()];
-                               ref.watch(replyrouteProvider.state).state=FirebaseFirestore.instance
-                                          .collection('groups')
-                                          .doc(ref.watch(groupidProvider.state).state)
-                                          .collection('chats')
-                                          .doc(ref.watch(replypathProvider.state).state)
+                              ref.watch(replyrouteProvider.state).state=await data['future']
+                                          .doc(ref.watch(replysProvider.state).state[i].id)
                                           .collection('reply');
-                              Map<String,dynamic> data={
-                                'text':ref.watch(daProvider.state).state[i]['text'],
-                                'date':ref.watch(daProvider.state).state[i]['date'],
-                                'uid':ref.watch(daProvider.state).state[i]['uid'],
-                                'username':ref.watch(unamesProvider.state).state[i],
-                                'path':ref.watch(daProvider.state).state[i].id,
-                                'future':ref.watch(replyrouteProvider.state).state
+                              //data['path']=await ref.watch(replysProvider.state).state[i].id;
+                              
+                              Map<String,dynamic> nextdata={
+                                'text':ref.watch(replysProvider.state).state[i]['text'],
+                                'date':ref.watch(replysProvider.state).state[i]['date'],
+                                'uid':ref.watch(replysProvider.state).state[i]['uid'],
+                                'username':ref.watch(replysunamesProvider.state).state[i],
+                                'path':ref.watch(replysProvider.state).state[i].id,
+                                'future':data['future']
+                                          .doc(ref.watch(replysProvider.state).state[i].id)
+                                          .collection('reply')
                               };
+                              
+                              ref.watch(replyPageProvider.state).state=0;
+                              ref.watch(replysProvider.state).state=[];
+                              //ref.watch(replyDateProvider.state).state=[DateTime.now().toLocal().toIso8601String()];
                               await Navigator.of(context).push(
                                 MaterialPageRoute(builder: (context){
-                                  return chatViewPage(data);
+                                  return chatViewPage(nextdata);
                                 })
                               ).then((value){
-                                ref.watch(replyrouteProvider.state).state="";
+                                //ref.watch(replypathProvider.state).state="";
+                                ref.watch(replyrouteProvider.state).state=data['future'];
                                 ref.watch(replysProvider.state).state=[];
                                 ref.watch(replysunamesProvider.state).state=[];
-                                ref.watch(replyDateProvider.state).state=[""];
+                                ref.watch(replyDateProvider.state).state=[DateTime.now().toLocal().toIso8601String()];
                                 ref.watch(replyPageProvider.state).state=0;
                               });
                             },
@@ -162,57 +238,6 @@ class ChatPage extends ConsumerWidget{
             ),
           ],
         ),
-      ),
-      drawer: Drawer(
-        child: Column(
-          children: <Widget>[
-            DrawerHeader(
-              child: Text(ref.watch(usernameProvider.state).state),
-            ),
-            ListTile(
-              title:Text("プロフィール"),
-              onTap: ()async{
-                final snapshot=await FirebaseFirestore.instance
-                                      .collection('users')
-                                      .doc(ref.watch(userProvider.state).state!.uid)
-                                      .get();
-                ref.watch(userinfoProvider.state).state=snapshot.data()!;
-                Navigator.of(context).pop();
-                await Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context){
-                    return ProfilePage();
-                  })
-                );
-              },
-            ),
-            Container(
-              alignment: Alignment.bottomCenter,
-              child:ListTile(
-                title:Text("ログアウト"),
-                  onTap: ()async{
-                    await FirebaseAuth.instance.signOut();
-                    ref.watch(userProvider.state).state=null;
-                    ref.watch(groupidProvider.state).state="";
-                    await Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) {
-                      return LoginPage();
-                    }),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child:Icon(Icons.add),
-        onPressed: ()async{
-          await Navigator.of(context).push(
-            MaterialPageRoute(builder:(context){
-              return AddPostPage();
-            }),
-          );
-        },
       ),
     );
   }
